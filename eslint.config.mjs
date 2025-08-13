f9fabd4d82c5a3482b9059676da89856795e47fc
BotFragger <m.ronikusuma@gmail.com>
2025-08-14 00:59:24 +0700

    prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,9 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -24,11 +17,13 @@ export default tseslint.config(
       },
     },
   },
+  eslintPluginPrettierRecommended,
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'linebreak-style': ['error', 'windows'],
     },
   },
 );
